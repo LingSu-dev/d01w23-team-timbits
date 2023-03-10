b5ce123314ae2f15edcd14aa3bf9f818bc1149d2
MeganMujiaLiu <megan13436702763@gmail.com>
2023-03-10 12:09:27 -0500

    fixed typo in variable name

--- deliverable3/pandas-1.5.3/pandas/io/json/_json.py
+++ deliverable3/pandas-1.5.3/pandas/io/json/_json.py
@@ -272,7 +272,7 @@ class FrameWriter(Writer):
             if isinstance(self.obj.columns, MultiIndex):
                 new_column = [list(x) for x in self.obj.columns]
                 # transposed =  list(map(list, zip(*list(map(list, zip(*self.obj.columns))))))
-                new_df = DataFrame(data=self.obj.values, columns=new_columns, index=self.obj.index)
+                new_df = DataFrame(data=self.obj.values, columns=new_column, index=self.obj.index)
                 obj_to_write = new_df
             else:
                 obj_to_write = self.obj
