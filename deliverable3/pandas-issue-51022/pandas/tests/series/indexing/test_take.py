8cb98f146538a100799c16baa86667f749bac513
Xu Zheng <barry.zheng@mail.utoronto.ca>
2023-03-11 21:44:25 -0500

    update acceptance tests

--- deliverable3/pandas-issue-51022/pandas/tests/series/indexing/test_take.py
+++ deliverable3/pandas-issue-51022/pandas/tests/series/indexing/test_take.py
@@ -31,34 +31,3 @@ def test_take_categorical():
         pd.Categorical(["b", "b", "a"], categories=["a", "b", "c"]), index=[1, 1, 0]
     )
     tm.assert_series_equal(result, expected)
-
-
-def test_take_wrong_axis():
-    # https://github.com/pandas-dev/pandas/issues/51022
-    ser = Series([1, 2, 3])
-
-    msg = lambda x: f"axis={x} is not a valid parameter"
-    with pytest.raises(ValueError, match=msg("foo")):
-        ser.take([1], axis="foo")
-    with pytest.raises(ValueError, match=msg(1)):
-        ser.take([1], axis=1)
-
-
-def test_take_non_default_axis():
-    # https://github.com/pandas-dev/pandas/issues/51022
-    msg = lambda x: f"axis={x} is not a valid parameter"
-    with pytest.raises(ValueError, match=msg("aaa")):
-        pd.Series([1, 4, 7]).take([2], axis="aaa")
-    with pytest.raises(ValueError, match=msg("0")):
-        pd.Series([1, 4, 7]).take([0, 1], axis="0")
-
-
-def test_take_default_axis():
-    # https://github.com/pandas-dev/pandas/issues/51022
-    actual = pd.Series([1, 4, 7]).take([2])
-    expected = pd.Series([7], index=[2])
-    tm.assert_series_equal(actual, expected)
-
-    actual = pd.Series([1, 4, 7]).take([0, 1], axis="index")
-    expected = pd.Series([1, 4], index=[0, 1])
-    tm.assert_series_equal(actual, expected)
