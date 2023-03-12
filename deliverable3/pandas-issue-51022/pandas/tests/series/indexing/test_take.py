c3af3136c5d4e931b4ec1ac8fcc948b7abaf3342
Xu Zheng <barry.zheng@mail.utoronto.ca>
2023-03-11 22:53:49 -0500

    move part of unit tests to a new test call

--- deliverable3/pandas-issue-51022/pandas/tests/series/indexing/test_take.py
+++ deliverable3/pandas-issue-51022/pandas/tests/series/indexing/test_take.py
@@ -61,6 +61,10 @@ def test_take_with_invalid_axis(self):
     with pytest.raises(ValueError, match=except_msg):
         s.take(indices, axis=ax)
 
+def test_take_with_2d_axis(self):
+    # https://github.com/pandas-dev/pandas/issues/51022
+    s = pd.Series([1, 2, 3, 4, 5])
+    indices = [0, 2, 4, 6]
     ax = 1
     except_msg = f"axis={ax} is not a valid parameter"
     with pytest.raises(ValueError, match=except_msg):
