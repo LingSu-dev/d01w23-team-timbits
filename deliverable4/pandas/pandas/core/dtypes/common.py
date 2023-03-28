8b782e7078ba5ea385d1a7a41f26e10968014553
Oliiiv <weng.zhang0708@gmail.com>
2023-03-28 01:34:00 -0400

    remove unnecessay part and revise as requested

--- deliverable4/pandas/pandas/core/dtypes/common.py
+++ deliverable4/pandas/pandas/core/dtypes/common.py
@@ -537,13 +537,13 @@ def is_ordered_categorical_dtype(arr_or_dtype) -> bool:
     """
     Check if the provided array or dtype is of an ordered categorical dtype.
     """
-    if arr_or_dtype is None or not CategoricalDtype.is_dtype(arr_or_dtype):
+    if arr_or_dtype is None or not is_categorical_dtype(arr_or_dtype):
         return False
     if isinstance(arr_or_dtype, ExtensionDtype):
         return arr_or_dtype.ordered
     else:
         cat_dtype = CategoricalDtype._from_values_or_dtype(values=arr_or_dtype)
-    return CategoricalDtype.is_dtype(cat_dtype) and cat_dtype.ordered
+    return cat_dtype.ordered
     
     # return CategoricalDtype.is_dtype(arr_or_dtype) and arr_or_dtype.ordered
 
@@ -551,13 +551,13 @@ def is_unordered_categorical_dtype(arr_or_dtype) -> bool:
     """
     Check if the provided array or dtype is of an unordered categorical dtype.
     """
-    if arr_or_dtype is None or not CategoricalDtype.is_dtype(arr_or_dtype):
-        return True
+    if arr_or_dtype is None or not is_categorical_dtype(arr_or_dtype):
+        return False
     if isinstance(arr_or_dtype, ExtensionDtype):
         return not arr_or_dtype.ordered
     else:
         cat_dtype = CategoricalDtype._from_values_or_dtype(values=arr_or_dtype)
-    return CategoricalDtype.is_dtype(cat_dtype) and not cat_dtype.ordered
+    return not cat_dtype.ordered
     
     # return CategoricalDtype.is_dtype(arr_or_dtype) and not arr_or_dtype.ordered
 
