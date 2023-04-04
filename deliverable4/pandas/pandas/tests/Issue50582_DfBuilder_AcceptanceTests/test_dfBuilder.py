84bb509a42e8018abb65c3137d7e8a2d4aff2ed7
coolxuzzz <barry.zheng@mail.utoronto.ca>
2023-04-03 20:51:00 -0400

    acceptance tests for asType()

--- deliverable4/pandas/pandas/tests/Issue50582_DfBuilder_AcceptanceTests/test_dfBuilder.py
+++ deliverable4/pandas/pandas/tests/Issue50582_DfBuilder_AcceptanceTests/test_dfBuilder.py
@@ -195,4 +195,108 @@ def test_dfBuilder_column_rows_mismatch_len():
 
     with pytest.raises(ValueError):
         builder.appendRow(['John', 30, 'M'])
+
+def test_dfBuilder_single_row_as_type_valid():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age'], dtypes=['str', 'int'])
+    expected = builder.appendRow(['John', 30]) \
+                        .asType(dtype = ['str', 'bool']) \
+                        .build()
+
+    # Create actual DataFrame using DataFrame constructor
+    columns = ["name", "age"]
+    dfs = []
+    dfs.append(pd.DataFrame([['John', 30]], columns=columns, dtype=object))
+    df_tmp = pd.concat(dfs, ignore_index=True)
+    actual = df_tmp.astype({"name": "str", "age": bool}, copy=False)
+
+    tm.assert_equal(actual, expected)
+
+def test_dfBuilder_multiple_rows_as_type_valid():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age', 'gender'], dtypes=['str', 'int', 'bool'])
+    expected = builder \
+                .appendRow(['John', 30, 'M']) \
+                .appendRow(['Mary', 25, 'F']) \
+                .appendRow(['Bob', 40, 'M']) \
+                .asType(dtype = ['str', 'str', 'bool']) \
+                .build()
+
+    # Create actual DataFrame using DataFrame constructor
+    columns = ['name', 'age', 'gender']
+    dfs = []
+    dfs.append(pd.DataFrame([['John', 30, 'M']], columns=columns, dtype=object))
+    dfs.append(pd.DataFrame([['Mary', 25, 'F']], columns=columns, dtype=object))
+    dfs.append(pd.DataFrame([['Bob', 40, 'M']], columns=columns, dtype=object))
+    df_tmp = pd.concat(dfs, ignore_index=True)
+    actual = df_tmp.astype({"name": "str", "age": "str", "gender": bool}, copy=False)
+
+    tm.assert_equal(actual, expected)
   
+def test_dfBuilder_multiple_rows_dict_as_type_valid():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age', 'gender'], dtypes=['str', 'int', 'bool'])
+    expected = builder \
+                .appendDict({"name": "John", "age": 30, "gender": "M"}) \
+                .appendDict({"name": "Mary", "age": 25, "gender": "F"}) \
+                .appendDict({"name": "Bob", "age": 40, "gender": "M"}) \
+                .asType(dtype = ['str', 'str', 'bool']) \
+                .build()
+
+    # Create actual DataFrame using DataFrame constructor
+    columns = ['name', 'age', 'gender']
+    dfs = []
+    dfs.append(pd.DataFrame([['John', 30, 'M']], columns=columns, dtype=object))
+    dfs.append(pd.DataFrame([['Mary', 25, 'F']], columns=columns, dtype=object))
+    dfs.append(pd.DataFrame([['Bob', 40, 'M']], columns=columns, dtype=object))
+    df_tmp = pd.concat(dfs, ignore_index=True)
+    actual = df_tmp.astype({"name": "str", "age": "str", "gender": bool}, copy=False)
+
+    tm.assert_equal(actual, expected)
+
+def test_dfBuilder_single_row_as_mismatch_type_str_to_int():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age'], dtypes=['str', 'int'])
+
+    with pytest.raises(ValueError):
+        builder.appendRow(['John', 30]).asType(dtype = ['int', 'int']).build()
+
+def test_dfBuilder_single_row_as_mismatch_type_str_to_float():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age'], dtypes=['str', 'int'])
+
+    with pytest.raises(ValueError):
+        builder.appendRow(['John', 30]).asType(dtype = ['float', 'int']).build()
+
+def test_dfBuilder_multiple_rows_as_mismatch_type():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age', 'gender'], dtypes=['str', 'int', 'str'])
+    
+    with pytest.raises(ValueError):
+        builder \
+        .appendRow(['BigGuy', 30, 'M']) \
+        .appendRow(['Mary', 25, 'F']) \
+        .appendRow(['Bob', 40, 'M']) \
+        .asType(dtype = ['int', 'int', 'str']) \
+        .build()
+
+def test_dfBuilder_single_row_as_type_mismatch_len():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age'], dtypes=['str', 'int'])
+
+    msg = "Given dtypes length do not match with columns length"
+    with pytest.raises(ValueError, match=msg):
+        builder.appendRow(['John', 30]).asType(dtype = ['int', 'int', 'int']).build()
+
+def test_dfBuilder_multiple_rows_as_type_mismatch_len():
+    # Create expected DataFrame using DataFrameBuilder
+    builder = dfBuilder(columns=['name', 'age', 'gender'], dtypes=['str', 'int', 'str'])
+    
+    msg = "Given dtypes length do not match with columns length"
+    with pytest.raises(ValueError, match=msg):
+        builder \
+        .appendRow(['John', 30, 'M']) \
+        .appendRow(['Mary', 25, 'F']) \
+        .appendRow(['Bob', 40, 'M']) \
+        .asType(dtype = ['int', 'int']) \
+        .build()
